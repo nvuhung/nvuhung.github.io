94a3719dc9a375ac35db131bd661b0982b3fe91c
Hung Nguyen <nvuhung@outlook.com>
2019-11-22 13:36:34 +0700

    Updates

--- precache-manifest.76f97c1731903d1901cf75babce8f962.js
+++ precache-manifest.76f97c1731903d1901cf75babce8f962.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "128b59c323353c969300532e96e57f89",
+    "revision": "0edcd8422be84a7fe801d74109c13bf6",
     "url": "/nvuhung.github.io/index.html"
   },
   {
-    "revision": "a71b02e589034d9e803e",
-    "url": "/nvuhung.github.io/static/css/main.d419d9c4.chunk.css"
+    "revision": "3f6e040927df4d123955",
+    "url": "/nvuhung.github.io/static/css/main.b4ba5114.chunk.css"
   },
   {
     "revision": "bd638c74010c9ff307d0",
     "url": "/nvuhung.github.io/static/js/2.2e088c17.chunk.js"
   },
   {
-    "revision": "a71b02e589034d9e803e",
-    "url": "/nvuhung.github.io/static/js/main.df620115.chunk.js"
+    "revision": "3f6e040927df4d123955",
+    "url": "/nvuhung.github.io/static/js/main.3e649b9f.chunk.js"
   },
   {
     "revision": "180c084513cddeed7dd6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nvuhung.github.io/precache-manifest.c3522378af2ca3c0a6a8130e8f81f895.js"
+  "/nvuhung.github.io/precache-manifest.76f97c1731903d1901cf75babce8f962.js"
 );
 
 self.addEventListener('message', (event) => {
