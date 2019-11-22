4e2aa18568f83338123a0c250d28742ecb3e3e1a
Hung Nguyen <nvuhung@outlook.com>
2019-11-22 12:40:52 +0700

    Update content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,205 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { data } from './constants'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Header = ({ data }) => (
+  <header className="resume-header pt-4 pt-md-0">
+    <div className="media flex-column flex-md-row">
+      <img className="mr-3 img-fluid picture mx-auto" src="/images/avatar.jpg" alt="" />
+      <div className="media-body p-4 d-flex flex-column flex-md-row mx-auto mx-lg-0">
+        <div className="primary-info">
+          <h1 className="name mt-0 mb-1 text-white text-uppercase text-uppercase">{data.name}</h1>
+          <div className="title mb-3">{data.title}</div>
+          <ul className="list-unstyled">
+            <li className="mb-2">
+              <a href={`mailto:${data.email}`}>
+                <i className="far fa-envelope fa-fw mr-2" data-fa-transform="grow-3"></i>
+                {data.email}
+              </a>
+            </li>
+            <li>
+              <a href={`tel:${data.phone}`}>
+                <i className="fas fa-mobile-alt fa-fw mr-2" data-fa-transform="grow-6"></i>
+                {data.phone}
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="secondary-info ml-md-auto mt-2">
+          <ul className="resume-social list-unstyled">
+            {(data.links || []).map(link => (
+              <li className="mb-3" key={link.link}>
+                <a href={link.link} rel="noopener noreferrer" target="_blank">
+                  <span className="fa-container text-center mr-2">
+                    <i className={link.icon}></i>
+                  </span>
+                  {link.content}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
-  );
-}
+  </header>
+)
 
-export default App;
+const Projects = ({ projects, title }) => (
+  <section className="resume-section experience-section mb-5">
+    <h2 className="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">{title}</h2>
+    <div className="resume-section-content">
+      <div className="resume-timeline position-relative">
+        {projects.map(item => (
+          <article className="resume-timeline-item position-relative pb-5" key={item.company + item.time}>
+            <div className="resume-timeline-item-header mb-2">
+              <div className="d-flex flex-column flex-md-row">
+                <h3 className="resume-position-title font-weight-bold mb-1">{item.title}</h3>
+                <div className="resume-company-name ml-auto">{item.company}</div>
+              </div>
+              <div className="resume-position-time">{item.time}</div>
+            </div>
+            <div className="resume-timeline-item-desc">
+              <p>{item.description}</p>
+              <ul>
+                {item.projects.map(project => (
+                  <li key={project}>
+                    <div dangerouslySetInnerHTML={{ __html: project }}></div>
+                  </li>
+                ))}
+              </ul>
+              <h4 className="resume-timeline-item-desc-heading font-weight-bold">Technologies used:</h4>
+              <ul className="list-inline">
+                {item.technologies_used.map(tech => (
+                  <li key={tech} className="list-inline-item">
+                    <span className="badge badge-primary badge-pill">{tech}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </article>
+        ))}
+      </div>
+    </div>
+  </section>
+)
+
+const Body = ({ data }) => (
+  <div className="resume-body p-5">
+    {data.career_summary && (
+      <section className="resume-section summary-section mb-5">
+        <h2 className="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Career Summary</h2>
+        <div className="resume-section-content">
+          <p className="mb-0">{data.career_summary}</p>
+        </div>
+      </section>
+    )}
+
+    <div className="row">
+      <div className="col-lg-9">
+        <Projects projects={data.work_experience} title="WORK EXPERIENCE" />
+      </div>
+
+      <div className="col-lg-3">
+        <section className="resume-section skills-section mb-5">
+          {/* Skill Tools */}
+          <h2 className="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Skills &amp; Tools</h2>
+          <div className="resume-section-content">
+            {(data.skills_tools || []).map(skill => (
+              <div className="resume-skill-item" key={skill.title}>
+                <h4 className="resume-skills-cat font-weight-bold">{skill.title}</h4>
+                <ul className="list-unstyled mb-4">
+                  {(skill.items || []).map(item => (
+                    <li className="mb-2" key={item.name}>
+                      <div className="resume-skill-name">{item.name}</div>
+                      <div className="progress resume-progress">
+                        <div
+                          className="progress-bar theme-progress-bar-dark"
+                          role="progressbar"
+                          style={{ width: `${item.level}%` }}
+                          aria-valuenow="25"
+                          aria-valuemin="0"
+                          aria-valuemax="100"
+                        ></div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+
+            {/* Others */}
+            <div className="resume-skill-item">
+              <h4 className="resume-skills-cat font-weight-bold">Others</h4>
+              <ul className="list-inline">
+                {data.others.map(item => (
+                  <li className="list-inline-item" key={item}>
+                    <span className="badge badge-light">{item}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </section>
+
+        {/* Education */}
+        <section className="resume-section education-section mb-5">
+          <h2 className="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Education</h2>
+          <div className="resume-section-content">
+            <ul className="list-unstyled">
+              {data.educations.map(item => (
+                <li className="mb-2" key={item.name + item.time}>
+                  <div className="resume-degree font-weight-bold">{item.name}</div>
+                  <div className="resume-degree-org">{item.description}</div>
+                  <div className="resume-degree-time">{item.time}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+
+        {/* Languages */}
+        <section className="resume-section language-section mb-5">
+          <h2 className="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">Language</h2>
+          <div className="resume-section-content">
+            <ul className="list-unstyled resume-lang-list">
+              {data.language.map(item => (
+                <li className="mb-2" key={item.name}>
+                  <span className="resume-lang-name font-weight-bold">{item.name}</span>{' '}
+                  <small className="text-muted font-weight-normal">({item.level})</small>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+
+        <section className="resume-section interests-section mb-5">
+          <h2 className="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">WORKING ATTITUDE</h2>
+          <div className="resume-section-content">
+            <ul className="list-unstyled">
+              {data.working_attitude.map(item => (
+                <li className="mb-1" key={item}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-9">
+        <Projects projects={data.other_projects} title="OTHER PROJECTS" />
+      </div>
+    </div>
+  </div>
+)
+
+const App = () => (
+  <article className="resume-wrapper text-center position-relative">
+    <div className="resume-wrapper-inner mx-auto text-left bg-white shadow-lg">
+      <Header data={data} />
+      <Body data={data} />
+    </div>
+  </article>
+)
+
+export default App
---
+++ src/constants/index.js
@@ -0,0 +1,205 @@
+export const data = {
+  pageTitle: 'Hung Nguyen',
+  name: 'Nguyễn Vũ Hưng',
+  title: 'Front End Developer',
+  email: 'nvuhung@outlook.com',
+  phone: '+(84) 38 2442 107',
+  links: [
+    {
+      link: 'https://linkedin.com/in/nvuhung',
+      content: 'linkedin.com/in/nvuhung',
+      icon: 'fab fa-linkedin-in fa-fw'
+    },
+    {
+      link: 'https://github.com/nvuhung',
+      content: 'github.com/nvuhung',
+      icon: 'fab fa-github-alt fa-fw'
+    },
+    {
+      link: 'skype:nvuhungit?chat',
+      content: 'nvuhungit',
+      icon: 'fab fa-skype'
+    }
+  ],
+  career_summary: '',
+  skills_tools: [
+    {
+      title: 'Front End',
+      items: [
+        {
+          name: 'React',
+          level: '95'
+        },
+        {
+          name: 'React Native',
+          level: '90'
+        },
+        {
+          name: 'Angular',
+          level: '90'
+        },
+        {
+          name: 'Javascript',
+          level: '90'
+        },
+        {
+          name: 'Typescript',
+          level: '85'
+        },
+        {
+          name: 'HTML/CSS/SASS/LESS',
+          level: '95'
+        }
+      ]
+    }
+  ],
+  others: [
+    'Git',
+    'Unit Testing',
+    'Code Review',
+    'GraphQL',
+    'Agile',
+    'Social APIs',
+    'Docker',
+    'Clean Code',
+    'Web Services',
+    'Design Pattern'
+  ],
+  educations: [
+    {
+      name: 'University of Technical and Education HoChiMinh City',
+      description: 'Information Technology',
+      time: '2010 - 2014'
+    }
+  ],
+  language: [
+    {
+      name: 'Vietnamese',
+      level: 'Native'
+    },
+    {
+      name: 'English',
+      level: 'Intermediate'
+    }
+  ],
+  working_attitude: [
+    'Active in communication and listener',
+    'Problem-solving skill in a short period of time',
+    'Patience, careful, smart and eager to learn new things',
+    'Quickly adaptable to new environments and techonologies',
+    'High sense of responsibility and humor',
+    'Good team work skill and able to work individually'
+  ],
+  work_experience: [
+    {
+      company: '',
+      title: 'Freelancer',
+      time: 'Sep 2019 - Present',
+      description:
+        'Work directly with a French developer and use React and React Native to to build 2 websites and 2 apps',
+      projects: [
+        "<strong>Paper</strong> - <a target='_blank' href='https://paperimmo.fr/'>https://paperimmo.fr/</a> - A real estate platform, receive the latest real estate listings in real time.",
+        "<strong>Lespeakers</strong> -  <a target='_blank' href='https://lespeakers.com/'>https://lespeakers.com/</a> - A platform linking conference and event organizer"
+      ],
+      technologies_used: ['React', 'React Native', 'Typescript', 'HTML/SASS', 'Redux', 'Firebase', 'Styled Components']
+    },
+    {
+      company: 'Ami',
+      title: 'Software Engineer',
+      time: 'Feb 2018 - Aug 2019',
+      description:
+        'Working at Ami, a startup company with the foundation of a real estate management system. With the desire to build an ecosystem for smart city, we have developed a lot of products, and these are the projects I have been involved in:',
+      projects: [
+        '<strong>Ami CA</strong>: Lead team Frontend with 3 members to build 3 apps at the same time: Citizen Mobile App, CA Mobile App (for Police) and CA Desktop (Web) App in only 1 month',
+        '<strong>Ami Exchange Online</strong>: Lead team Frontend with 3 members to build the real estate webapp to help users find houses, condominiums, apartments for sale and rent',
+        '<strong>Ami City Blog</strong>: Using Ghost to build a blog website for Ami City in 1 week',
+        '<strong>Ami Rooms</strong>: Setup the project structure and do some task for Ami Rooms. This website can help everyone find the room to rent.',
+        '<strong>Ami Landlord</strong>: Build the web app for Landlord and Manager. It will provide the solutions to manage properties and citizen by technology, help the Landlord optimize the process, manage professionally - effectively, save time – cost',
+        '<strong>Ami Electricity App</strong>: The meter will send number of kwh to this app. So that users can track, view report and know which device is using the most power',
+        '<strong>AMA App</strong>: Ami A is a platform for SMEs to record supply chain data, monitor and authenticate products based on Blockchain, Smartcontract, Big Data technology ... This app was used in Tu Hung cup',
+        '<strong>Ami Building</strong>: Build the web app for Building Manager. It will provide the solutions to manage building and residents by technology, help the Management optimize the process, manage professionally - effectively, save time – cost',
+        '<strong>Building Staff</strong>: Implementing the app for staff of building. With this, staffs can receive notification from citizen and manager about the issue of apartment or building. Update issue status and schedule tasks status.',
+        '<strong>Ami Hero</strong>: Working with Hero Mobile App, that is the app for the maintenance man. This app will help them to receive the issues which are reported from renter, change issue status, send feedback, estimate price, report to landlord and renter'
+      ],
+      technologies_used: [
+        'React',
+        'React Native',
+        'Typescript',
+        'HTML/SASS/LESS',
+        'Redux',
+        'Firebase',
+        'Styled Components',
+        'GraphQL',
+        'NodeJS',
+        'Storybook',
+        'NextJS',
+        'NestJS',
+        'Electron',
+        'Angular',
+        'Docker',
+        'ChartJS'
+      ]
+    },
+    {
+      company: 'Spiraledge',
+      title: 'Senior Front End Developer',
+      time: 'Jan 2016 - Feb 2018',
+      description: 'Became Senior Front End Developer after 1 year, I joined some projects:',
+      projects: [
+        '<strong>Practyce</strong>: Working with 1 backend developer to build Practyce website, help everyone can search, learn and practice yoga at home with expert yoga instructors.',
+        '<strong>Tend</strong>: The first software platform with advanced data analytics specifically designed for diversified and small- to mid-sized organic farms'
+      ],
+      technologies_used: ['AngularJS', 'HTML/CSS/LESS', 'Typescript', 'Angular', 'JQuery', 'Gulp', 'Google Charts']
+    },
+    {
+      company: 'CSC',
+      title: 'Front End Developer',
+      time: 'Sep 2014 - Jan 2016',
+      description:
+        'Passed Java Fresher at CSC with the best scores and then join a team with over 50 members. In additional to being a Front End Developer, I also became an trainers to train newcomers. Here are some projects I joined:',
+      projects: [
+        '<strong>iPOS</strong>: A portal system for insurance, it handles the complex calculations, and transaction for insurance products.',
+        '<strong>eTiQa</strong>: In only 4 months, 2 other developers and I built built successfully DPI Website for client to buy online insurance product. This is one of first direct-sale website for an Insurance Company in Singapore.',
+        '<strong>Tokio Marine</strong>: Building new products for Tokio Marine – an insurance company in Japan. Working with a team of 3 developers and 1 QC for only 2 weeks. We built 2 new products successfully along with over 5 other products developed earlier.'
+      ],
+      technologies_used: ['AngularJS', 'HTML/CSS', 'JQuery', 'Java', 'SpringMVC', 'XML', 'MySQL']
+    }
+  ],
+  other_projects: [
+    {
+      company: 'https://www.mouldflo.com/',
+      title: 'Mouldflo',
+      time: 'Nov 2019 - Present',
+      description:
+        'Working with a manager from Denmark and 2 other developers in Vietnam, we built an admin page for Mouldflow',
+      projects: [],
+      technologies_used: ['Angular', 'Typescript', 'HTML/SASS']
+    },
+    {
+      company: 'https://deskpro.github.io/',
+      title: 'Deskpro',
+      time: 'Otc 2019 - Nov 2019',
+      description:
+        'Building UI components with styled components and storybook. Receive tasks from Mexican developer and working along with some developers in others countries.',
+      projects: [],
+      technologies_used: ['React', 'Typescript', 'Styled Components', 'Storybook']
+    },
+    {
+      company: 'https://tourhawaiiapp.com/',
+      title: 'Tour Hawaii',
+      time: 'Sep 2019 - Otc 2019',
+      description:
+        'A small app for tours in Hawaii from an American client. With this app we can check available tours and pay online with Paypal',
+      projects: [],
+      technologies_used: ['React Native', 'Typescript', 'Paypal SDK', 'Express']
+    },
+    {
+      company: '',
+      title: 'Viral Works',
+      time: 'Apr 2019 - Jul 2019',
+      description: 'Working as a backend developer. Building a web application for influencers',
+      projects: [],
+      technologies_used: ['NestJS', 'Typescript', 'MongoDB', 'Social APIs']
+    }
+  ]
+}
